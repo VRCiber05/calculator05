e02634238d5c6bd636f4876e533f473b431b0e26
Veronica <TJF85432@educastur.es>
2024-04-25 17:05:36 +0200

    Añadido Jenkinsfile

---
+++ src/main/java/com/ciber05/calculator05/CalculatorApplicationTests.java
@@ -0,0 +1,24 @@
+package com.ciber05.calculator05;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootTest
+public class CalculatorApplicationTests {
+    private Calculator calculator = new Calculator();
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void testSum() {
+   
+    }
+
+
+
+    
+}
---
+++ src/main/java/com/ciber05/calculator05/SpringBootTest.java
@@ -0,0 +1,5 @@
+package com.ciber05.calculator05;
+
+public @interface SpringBootTest {
+
+}
